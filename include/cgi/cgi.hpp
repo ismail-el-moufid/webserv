82c78e15c8da700875d1b601876c7a6093d58cc2
Abdelkader37 <Qrafiabdelkader@gmail.com>
2026-04-30 15:46:22 +0100

    update cgi

--- include/cgi/cgi.hpp
+++ include/cgi/cgi.hpp
@@ -3,31 +3,45 @@
 #include "../config/Route.hpp"
 #include "../config/VirtualHost.hpp"
 #include "../http/HttpRequest.hpp"
+#include "../http/HttpResponse.hpp"
 #include "../core/Pipe.hpp"
 #include <vector>
 #include <unistd.h>
+#include "utils/NetworkUtils.hpp"
+#include <iostream>
 
-
-class CGI
+class CGIProcess
 {
-    private:
-    std::vector<std::string> _env;
-    std::vector<std::string> _arg;
-    std::vector<const char*>  _argv;
-    std::vector<const char*>  _envm;
-    std::string _respond;
+public:
+    CGIProcess();
 
-    int status;
-    public:
-        CGI();
-        ~CGI();
-        std::string setenv(const HttpRequest &request, const VirtualHost &vhost, const Route &route);
-        void setarg(std::string filename, const Route &route);
-        char **getenv();
-        char **getarg();
-        void execute_cgi(const HttpRequest &request);
+    bool running() const;
+
+    Pipe        stdin_;
+    Pipe        stdout_;
+    pid_t       pid_;
+    time_t      started_;
+    std::string output_buffer_;
+    ssize_t      bytes_sent_;
+
+private:
+    CGIProcess(const CGIProcess &);
+    CGIProcess &operator=(const CGIProcess &);
+    ~CGIProcess();
 };
 
+class CGIHandler
+{
+public:
+    static void start(CGIProcess &cgi, const HttpRequest &request, const Interface &iface);
+    static void writeStdin(CGIProcess &cgi, const HttpRequest &request);
+    static void readStdout(CGIProcess &cgi);
+    static void finish(CGIProcess &cgi, HttpResponse &response);
+    static void kill(CGIProcess &cgi, HttpResponse &response);
 
-
-void cgi(const HttpRequest &request, const VirtualHost &vhost, const Route &route);
+private:
+    static std::vector<std::string> buildEnv(const HttpRequest &request, const Interface &iface);
+    static std::vector<std::string> buildArg(const std::string &filename, const HttpRequest &request);
+    static char                   **toCharArray(const std::vector<std::string> &vec);
+    static std::string              resolveScript(const HttpRequest &request, std::vector<std::string> &env);
+};
